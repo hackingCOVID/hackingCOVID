d3b6c630b404b1b8d6f956212ad2fdc96bb2ca6a
Maria <marybelousova@yahoo.com>
2021-02-07 15:14:59 -0500

    add 2 options for navbar

--- client/AppNavbar.js
+++ client/AppNavbar.js
@@ -10,40 +10,40 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 function AppNavbar() {
     return (
         <div>
-      <div>
-          <Navbar bg="primary" variant="dark">
-            <Navbar.Brand href="#home">HackingCOVID</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="#home">Home</Nav.Link>
-                <Nav.Link href="#features">Map</Nav.Link>
-                <Nav.Link href="#pricing">Vaccine Info</Nav.Link>
-            </Nav>
-            <Form inline>
-                <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-                <Button variant="outline-light">Search</Button>
-            </Form>
-        </Navbar>
+          <div>
+            <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" className="justify-center" >
+                <Navbar.Brand href="#home">HackingCOVID</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="ml-auto">
+                        <Nav.Link href="#features">Home</Nav.Link>
+                        <Nav.Link href="#pricing">Map</Nav.Link>
+                        <Nav.Link href="#pricing">Vaccine Info</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
+            </div>
+            <div>
+            <Navbar bg="primary" variant="dark">
+                  <Navbar.Brand href="#home">HackingCOVID</Navbar.Brand>
+                  <Nav className="mr-auto">
+                      <Nav.Link href="#home">Home</Nav.Link>
+                      <Nav.Link href="#features">Map</Nav.Link>
+                      <NavDropdown title="Vaccine Info" id="collasible-nav-dropdown" className="mr-auto" >
+                            <NavDropdown.Item href="#action/3.1">Different vaccines</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Phase</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Locations</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                        </NavDropdown>
+                  </Nav>
+                  <Form inline>
+                      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                      <Button variant="outline-light">Search</Button>
+                  </Form>
+              </Navbar>
+            </div>
     </div>
-      <div>
-      <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark" className="justify-end" >
-  <Navbar.Brand href="#home">HackingCOVID</Navbar.Brand>
-  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-  <Navbar.Collapse id="responsive-navbar-nav">
-    <Nav className="mr-auto">
-      <Nav.Link href="#features">Home</Nav.Link>
-      <Nav.Link href="#pricing">Map</Nav.Link>
-      <NavDropdown title="Vaccine Info" id="collasible-nav-dropdown">
-        <NavDropdown.Item href="#action/3.1">Different vaccines</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.2">Phase</NavDropdown.Item>
-        <NavDropdown.Item href="#action/3.3">Locations</NavDropdown.Item>
-        <NavDropdown.Divider />
-        <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-      </NavDropdown>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-</div>
-</div>
     )
 }
 
