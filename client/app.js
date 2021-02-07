5e428ffc6cf0e9cb4123c3bf0b7ff37a657513e8
Maria <marybelousova@yahoo.com>
2021-02-07 15:02:36 -0500

    create a navbar

---
+++ client/app.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import AppNavbar from './AppNavbar'
+
+function App() {
+    return (
+      <div>
+          <AppNavbar/>
+      </div>
+    );
+  }
+  
+  export default App;
+  
